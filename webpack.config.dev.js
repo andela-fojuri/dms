56220abebbfc1878dc4830a0aba4c82d7bdb54f4
fisayomi <fisayomi.ojuri@andela.com>
2017-03-31 11:54:52 +0100

    include webpack configurations

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,19 +16,37 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
+
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+
   devServer: {
     contentBase: path.resolve(__dirname, 'client')
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Hammer: 'hammerjs/hammer'
+    }),
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      jquery: path.join(__dirname, 'node_modules/jquery/dist/jquery'),
+    }
+  },
   module: {
     loaders: [
       { test: /\.js$/, include: path.join(__dirname, 'client'), loaders: ['babel'] },
       { test: /(\.css)$/, loaders: ['style', 'css'] },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
       {
@@ -37,7 +55,15 @@ export default {
         options: {
           limit: 25000,
         },
-      }
+      },
+      { test: /materialize-css\/bin\//, loader: 'imports?jQuery=jquery,$=jquery,hammerjs' },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
     ]
   }
 };
